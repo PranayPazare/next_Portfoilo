f6f563ebf2ce4fdff07ab0e5c934882daab36c81
PranayPazare <97777549+PranayPazare@users.noreply.github.com>
2026-06-29 18:51:07 +0530

    Add a little top padding to the banner figure

--- src/features/portfolio/components/profile-header.tsx
+++ src/features/portfolio/components/profile-header.tsx
@@ -10,7 +10,7 @@ import { VerifiedIcon } from "./verified-icon"
 export function ProfileHeader() {
   return (
     <div className="screen-line-bottom grid grid-cols-[auto_1fr] grid-rows-[1fr_auto] overflow-y-clip border-x border-line">
-      <figure className="relative col-span-2 p-2 sm:col-span-1 sm:col-start-2 sm:p-4">
+      <figure className="relative col-span-2 p-2 pt-6 sm:col-span-1 sm:col-start-2 sm:p-4 sm:pt-8">
         <ChanhDaiMarkIsometric />
         <figcaption className="pointer-events-none absolute right-2 bottom-2 font-mono text-xs leading-none text-zinc-400 select-none sm:right-4 dark:text-zinc-700">
           FIG_001
